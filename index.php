7b556f93b6f0be09433a8b771600d2cfab36366a
Jhonathan Gonzlez Aguirre <jota_q91@hotmail.com>
2020-05-21 21:22:17 -0500

    se agregar el botón para editar denuncias. Pendiente programar el botón.

--- index.php
+++ index.php
@@ -76,6 +76,7 @@
                     <th>Placa</th>
                     <th>Denuncia</th>
                     <th></th>
+                    <th></th>
                 </tr>
                 <?php
                 $servername = "localhost";
@@ -108,6 +109,7 @@
                     <td> <?php echo $row['tipo']; ?></td>
                     <td> <?php echo $row['placa']; ?></td>
                     <td> <?php echo $row['denuncia']; ?></td>
+                    <td><a href="editarDenuncia.php?id_para_editar=<?php echo $row['id_pk']; ?>">Editar</a></td>
                     <td><a href="eliminarDenuncia.php?id_para_borrar=<?php echo $row['id_pk']; ?>">eliminar</a></td>
                 </tr>
                 <?php
